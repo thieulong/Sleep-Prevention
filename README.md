395eb6bf27e7b0f20060760c90b6b27ed1b18cba
thieulong <thieulongph@gmail.com>
2021-06-06 22:31:26 +0700

    Done

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Sleep-Prevention
 A program using your device's webcam to track your face and eyes to prevent you from falling as sleep while focusing on doing your work.
+### 1. Clone the project
+You'll first need to clone this project by running the following command  
+
+`git clone https://github.com/thieulong/Sleep-Prevention.git`  
+
+**Note**: Make sure you have Github installed on your system. If not, check out [***this site***](https://gist.github.com/derhuerst/1b15ff4652a867391f03)  
+
+### 2. Install requirements
+To run the code file, some of the requirements need to be installed, run this command (in this Github directory) to install all of the neccessary requirements.
+`pip3 install requirements.txt`
+  
+### 3. Run the project
+Navigate to this Github directory, run
+`python3 sleep_detection.py`
+and the project will start using your device's webcam to track your head and eye to make sure you're not falling as sleep while working.
+  
+**Note**: If you encounter a *Can't open camera by index* error, try to change the camera index base on your device's webcam index (Usually happends when you have another plug-in webcam)
+  
+Change the camera index (line 7) in *sleep_detection.py* file. If you have no idea what your camera index is, try to replace 0 with any number from 1 and going up.
+  
+You can minimize the tracking window while you're working.
---
+++ sleep_detection.py
@@ -0,0 +1,50 @@
+import cv2 as cv
+from pygame import mixer
+
+mixer.init()
+mixer.music.load('beep.wav')
+
+camera_id = 0
+
+camera = cv.VideoCapture(camera_id)
+
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+eye_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_eye_tree_eyeglasses.xml')
+
+while True:
+
+    ret, frame = camera.read()
+
+    if ret:
+
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        face = face_cascade.detectMultiScale(gray, 1.05, 15, minSize=(50, 50))
+
+        eye = eye_cascade.detectMultiScale(gray, 1.05, 8, minSize=(10, 10))
+
+        face_detect = 0
+
+        eye_detect = 0
+
+        for (x, y, w, h) in face:
+            cv.rectangle(img=frame, pt1=(x, y), pt2=(x+w, y+h), color=(0, 0, 255), thickness=5)
+            cv.putText(frame, "Face Detected", (x + 5, y + 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            face_detect += 1
+
+        for (x, y, w, h) in eye:
+            cv.rectangle(img=frame, pt1=(x, y), pt2=(x+w, y+h), color=(255, 0, 255), thickness=3)
+            cv.putText(frame, "Eye Detected", (x + 5, y + 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            eye_detect += 1
+
+        if face_detect == 0 and eye_detect == 0:
+            mixer.music.play()
+
+        cv.imshow("Sleep Detection", frame)
+
+    if cv.waitKey(1) & 0xFF == 32:
+        break
+
+camera.release()
+cv.destroyAllWindows()
